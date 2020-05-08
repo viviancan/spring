64400261f6419a56da46b16df8e1bc882cb47066
viviancan <vcnales@gmail.com>
2020-05-08 11:33:37 -0500

    Initial commit

---
+++ src/main/java/com/codeup/CodeupSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupSpringApplication.class, args);
+    }
+
+}
