b9e3525e4b41dd5d35ce0e222ebe62032905f541
viviancan <vcnales@gmail.com>
2020-05-11 21:04:33 -0500

    Added partials for head, navbar, and scripts(bootstrap)

--- src/main/java/com/codeup/controllers/HelloController.java
+++ src/main/java/com/codeup/controllers/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Controller
 class HelloController {
     @GetMapping("/hello")
@@ -29,4 +32,31 @@ class HelloController {
         model.addAttribute("cohort", "Welcome to " + cohort + "!");
         return "join";
     }
+
+    @GetMapping("/roll-dice")
+    public String rollDice(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{userNum}")
+    public String rollDiceReturn(@PathVariable int userNum, Model model){
+        boolean isEqual = false;
+
+//        https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+        int randomNumber = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+
+        if(randomNumber == userNum){
+            isEqual = true;
+        }
+
+        model.addAttribute("userNum", userNum);
+        model.addAttribute("isEqual", isEqual);
+        model.addAttribute("randomNum", randomNumber);
+
+        return "roll-dice-answer";
+    }
+
+
+
+
 }
